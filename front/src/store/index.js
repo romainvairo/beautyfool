48dbf8495eba5234a8a0f1ac7b502ee38dab7e9f
zougui <zouguidu91@gmail.com>
2019-11-26 14:10:24 +0100

    fix import middlewares

--- front/src/store/index.js
+++ front/src/store/index.js
@@ -1,7 +1,7 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 
 import reducers from './reducers';
-import middlewares from './middlewares';
+import * as middlewares from './middlewares';
 
 let devtools;
 
