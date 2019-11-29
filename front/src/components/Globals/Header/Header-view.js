58c951de84aa8c5a14f73c665b1532af75223732
romainvairo <romain.vairo@hotmail.com>
2019-11-29 17:59:54 +0100

    Footer, Header, Login, Navbar

--- front/src/components/Globals/Header/Header-view.js
+++ front/src/components/Globals/Header/Header-view.js
@@ -1,13 +1,28 @@
 import React from 'react';
 
+import './Header.scss';
+
 const HeaderView = ({ translations }) => (
   <header className="Header">
+    <ul className="burger">
+      <li className="burger">hey</li>
+      <li>hi</li>
+      <li>moune</li>
+      <li>miam</li>
+      <li>miou</li>
+      <li>ajouka</li>
+      <li>aboook</li>
+      <li>riahanna</li>
+    </ul>
     <div className="Header-Logo">
       <img src={require('../../../assets/images/logo.png')} alt="Logo" />
     </div>
     <p>Slogan</p>
     <div className="Header-SocialNetworkLinks">
       <i className="fab fa-facebook"></i>
+      <i className="fab fa-twitter"></i>
+      <i className="fab fa-instagram"></i>
+      <i className="fab fa-share-alt"></i>
     </div>
   </header>
 );
