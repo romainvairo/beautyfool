ff1f8143fe0cb33d2623f3af162cdc8a6a438a13
FrancoisRocket <darbon.francois@gmail.com>
2019-11-29 21:27:36 +0100

    header design 2

--- front/src/components/Globals/Header/Header-view.js
+++ front/src/components/Globals/Header/Header-view.js
@@ -4,14 +4,16 @@ import './Header.scss';
 const HeaderView = ({ translations }) => (
   <header className="Header">
     <div className="Header-Logo">
-      <img src={require('../../../assets/images/logo.png')} alt="Logo" />
+      <a href="">
+        <img src={require('../../../assets/images/logo.png')} alt="Logo" />
+      </a>
     </div>
     <p className="Header-Slogan">{translations.slogan}</p>
     <div className="Header-SocialNetworkLinks">
-      <i className="fab fa-facebook"></i>
-      <i className="fab fa-twitter"></i>
-      <i className="fab fa-instagram"></i>
-      <i className="fas fa-share-alt"></i>
+      <a className="Header-SocialNetworkLinks-Link" href=""><i className="fab fa-facebook"></i></a>
+      <a className="Header-SocialNetworkLinks-Link" href=""><i className="fab fa-twitter"></i></a>
+      <a className="Header-SocialNetworkLinks-Link" href=""><i className="fab fa-instagram"></i></a>
+      <a className="Header-SocialNetworkLinks-Link" href=""><i className="fas fa-share-alt"></i></a>
     </div>
     <div className="Header-Login">
       <a className="Header-Login-Link" href="">
