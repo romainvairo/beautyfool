2a1811be405ebfbc54997719943db2edc311e1fc
FrancoisRocket <darbon.francois@gmail.com>
2019-12-03 16:20:51 +0100

    layout header mobile first 1

--- front/src/components/Globals/Header/Header-view.js
+++ front/src/components/Globals/Header/Header-view.js
@@ -1,27 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Grid, Paper} from '@material-ui/core';
 
 import './Header.scss';
+import Dropdown from './Dropdown';
 
 const HeaderView = ({ translations }) => (
   <header className="Header">
-    <div className="Header-Logo">
-      <Link to="/">
-        <img src={require('../../../assets/images/logo.png')} alt="Logo" />
-      </Link>
-    </div>
-    <p className="Header-Slogan">{translations.slogan}</p>
-    <div className="Header-SocialNetworkLinks">
-      <Link className="Header-SocialNetworkLinks-Link" to=""><i className="fab fa-facebook"></i></Link>
-      <Link className="Header-SocialNetworkLinks-Link" to=""><i className="fab fa-twitter"></i></Link>
-      <Link className="Header-SocialNetworkLinks-Link" to=""><i className="fab fa-instagram"></i></Link>
-      <Link className="Header-SocialNetworkLinks-Link" to=""><i className="fas fa-share-alt"></i></Link>
-    </div>
-    <div className="Header-Login">
-      <Link className="Header-Login-Link" to="/profile">
-        <i className="fas fa-user"></i>{translations.login}
-      </Link>
-    </div>
+    <Grid container justify="space-between">
+      <Grid xs={3} sm={2} item>
+        <Dropdown />
+      </Grid>
+      <Grid xs={6} sm={8} container item alignItems="center" direction="column" className="Header-Logo">
+        <Grid item>
+          <Link to="/">
+            <img className="Header-Logo" src={require('../../../assets/images/logo.png')} alt="Logo" />
+          </Link>
+        </Grid>
+        <Grid item>
+          <p className="Header-Slogan">{translations.slogan}</p>
+        </Grid>
+      </Grid>
+      <Grid xs={3} sm={2} container item justify="flex-end">
+        <Grid item className="pr-2">
+          <Link className="Header-Login" to="/profile">
+            <i className="fas fa-user block text-center"></i>
+            <span className="block text-center text-xs">
+              Mon compte
+            </span>
+          </Link>
+        </Grid>
+      </Grid>
+    </Grid>
   </header>
 );
 
