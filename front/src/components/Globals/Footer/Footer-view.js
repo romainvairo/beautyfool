748b0d6f8219e2b634526d4c1f2584a950b4a8cd
FrancoisRocket <darbon.francois@gmail.com>
2019-12-11 14:26:53 +0100

    correction footer

--- front/src/components/Globals/Footer/Footer-view.js
+++ front/src/components/Globals/Footer/Footer-view.js
@@ -8,10 +8,10 @@ const FooterView = ({ translations }) => (
 
   <footer>
     <Grid className="socialNetwork" align="center">
-      <Link className="Header-SocialNetworkLinks-Link" to=""><i className="fab fa-facebook"></i></Link>
-      <Link className="Header-SocialNetworkLinks-Link" to=""><i className="fab fa-twitter"></i></Link>
-      <Link className="Header-SocialNetworkLinks-Link" to=""><i className="fab fa-instagram"></i></Link>
-      <Link className="Header-SocialNetworkLinks-Link" to=""><i className="fas fa-share-alt"></i></Link>
+      <Link className="Header-SocialNetworkLinks-Link" target="_blank" to=""><i className="fab fa-facebook"></i></Link>
+      <Link className="Header-SocialNetworkLinks-Link" target="_blank" to=""><i className="fab fa-twitter"></i></Link>
+      <Link className="Header-SocialNetworkLinks-Link" target="_blank" to=""><i className="fab fa-instagram"></i></Link>
+      <Link className="Header-SocialNetworkLinks-Link" target="_blank" to=""><i className="fas fa-share-alt"></i></Link>
     </Grid>
     <Grid container justify="space-between" className="Footer text-center">
       <Grid xs={12} sm={6} md="auto" item>
