9e7596efa567f386bebdf9304b0cce9f7534c760
FrancoisRocket <darbon.francois@gmail.com>
2019-12-12 16:42:53 +0100

    merge romain jeremy 2

--- front/src/components/Pages/Gallery/Gallery-view.js
+++ front/src/components/Pages/Gallery/Gallery-view.js
@@ -3,7 +3,6 @@ import React from 'react';
 import './Gallery.scss';
 
 const GalleryView = ({ images, translations })  => (
-<<<<<<< HEAD
 
   <section className="Gallery">
     {images.map(( image ) => (
@@ -12,14 +11,6 @@ const GalleryView = ({ images, translations })  => (
         <img className="Gallery-Images" key={image} src={image} alt="" />
       </section>
 
-=======
-  <section className="GalleryCarousel">
-    {images.map((image, i) => (
-      <section className="Gallery-Subsection">
-        <img className="Gallery-Images" key={image} src={image} alt="" />
-        <p className="Gallery-Paragraph">{translations[i]}</p>
-      </section>
->>>>>>> jeremy
     ))}
   </section>
 );
