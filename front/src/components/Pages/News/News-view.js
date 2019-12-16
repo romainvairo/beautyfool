fc0691523ae5195866efbbe9fdafe3cd9466ab17
FrancoisRocket <darbon.francois@gmail.com>
2019-12-16 13:36:42 +0100

    import data of actualities in views with map

--- front/src/components/Pages/News/News-view.js
+++ front/src/components/Pages/News/News-view.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { TextField, Button, Grid, Typography, FormControl, Select, InputLabel } from '@material-ui/core';
 
-const NewsView = ({ translations }) => (
+const NewsView = ({ translations, news }) => (
   <>
-  <Grid container justify="center">
-    <Grid item className="mt-6 mb-6">
-      <Typography variant="h4">{ translations.title }</Typography>
-    </Grid>
-    <Grid container item justify="center">
-      <Grid xs={6} lg={9} item>
-        <FormControl fullWidth variant="filled">
-          <Grid item container>
-            <InputLabel>Categorie</InputLabel>
+    <Grid container justify="center">
+      <Grid item className="mt-6 mb-6">
+        <Typography variant="h4">{ translations.title }</Typography>
+      </Grid>
+      <Grid container item justify="center">
+        <Grid xs={6} lg={9} item className="text-center">
+          <FormControl variant="filled" className="w-64">
+            <Grid item container>
+              <InputLabel>Categorie</InputLabel>
               <Select
                 fullWidth
                 native
@@ -22,54 +22,30 @@ const NewsView = ({ translations }) => (
                 }}
               >
                 <option value={translations.category} />
-                <option value={10}></option>
-                <option value={20}>Twenty</option>
-                <option value={30}>Thirty</option>
+                <option value={20}>{translations.nails}</option>
+                <option value={30}>{translations.hairRemoval}</option>
+                <option value={30}>{translations.handsAndFeet}</option>
+                <option value={30}>{translations.makeup}</option>
+                <option value={30}>{translations.face}</option>
+                <option value={30}>{translations.body}</option>
               </Select>
-          </Grid>
-        </FormControl>
+            </Grid>
+          </FormControl>
+        </Grid>
       </Grid>
     </Grid>
-  </Grid>
-  <Grid xs={12} lg={12} container item spacing={2} className="mt-10 mb-10">
-  <Grid xs={6} md={4} item>
-    <img src={require("../../../assets/images/makeup_1.jpg")}/>
-  </Grid>
-  <Grid xs={6} md={4} item>
-    <img src={require("../../../assets/images/makeup_1.jpg")}/>
-  </Grid>
-  <Grid xs={6} md={4} item>
-    <img src={require("../../../assets/images/makeup_1.jpg")}/>
-  </Grid>
-  <Grid xs={6} md={4} item>
-    <img src={require("../../../assets/images/makeup_1.jpg")}/>
-  </Grid>
-  <Grid xs={6} md={4} item>
-    <img src={require("../../../assets/images/makeup_1.jpg")}/>
-  </Grid>
-  <Grid xs={6} md={4} item>
-    <img src={require("../../../assets/images/makeup_1.jpg")}/>
-  </Grid>
-  <Grid xs={6} md={4} item>
-    <img src={require("../../../assets/images/makeup_1.jpg")}/>
-  </Grid>
-  <Grid xs={6} md={4} item>
-    <img src={require("../../../assets/images/makeup_1.jpg")}/>
-  </Grid>
-  <Grid xs={6} md={4} item>
-    <img src={require("../../../assets/images/makeup_1.jpg")}/>
-  </Grid>
-  <Grid xs={6} md={4} item>
-    <img src={require("../../../assets/images/makeup_1.jpg")}/>
-  </Grid>
-  <Grid xs={6} md={4} item>
-    <img src={require("../../../assets/images/makeup_1.jpg")}/>
-  </Grid>
-  <Grid xs={6} md={4} item>
-    <img src={require("../../../assets/images/makeup_1.jpg")}/>
-  </Grid>
-</Grid>
-</>
+    <Grid xs={12} lg={12} container item spacing={2} className="mt-10 mb-10">
+      {news.map((newA) => (
+        <div key={newA}>
+          <Grid xs={6} md={4} item>
+            <img src={require("../../../assets/images/makeup_1.jpg")}/>
+            <h1>{newA.title}</h1>
+            <p>{newA.content}</p>
+          </Grid>
+        </div>
+      ))}
+    </Grid>
+  </>
 );
 
 export default NewsView;
