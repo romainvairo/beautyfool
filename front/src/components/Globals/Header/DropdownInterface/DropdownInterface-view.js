223a82075370be6945391adf082ca871b983d809
romainvairo <romain.vairo@hotmail.com>
2019-12-17 09:52:59 +0100

    Dropdown not ending

--- front/src/components/Globals/Header/DropdownInterface/DropdownInterface-view.js
+++ front/src/components/Globals/Header/DropdownInterface/DropdownInterface-view.js
@@ -10,29 +10,29 @@ import translations from './translations';
 import Dropdown from './Dropdown.scss';
 
 const DropdownInterfaceView = ({ translations, onClose, onClick }) => (
-  <Grid container className="dropdown">
-   <Grid xs={12} item className="closeButton text-right">
-            <IconButton onClick={onClose}>
-              <CloseIcon />
-            </IconButton>
-        </Grid>
-          <div className="link">
-            <i class="fas fa-home text-4xl"></i><Link className="icons" to="/">Accueil</Link>
-          </div>
-            <i class="far fa-newspaper text-4xl"></i><Link className="icons" to ="/news">Actualités</Link>
-          <div className="link">
-            <i class="fas fa-clipboard-list ml-2 text-4xl"></i><Link className="icons ml-1" to ="/benefits">Prestations</Link>
-          </div>
-          <div className="link">
-            <i class="fas fa-paint-brush text-4xl"></i><Link className="icons" to ="/beauty-tips">Astuces beauté</Link>
-          </div>
-          <div className="link">
-            <i class="fas fa-image pl-2 text-4xl"></i><Link className="icons" to ="/gallery">Galerie</Link>
-          </div>
-          <div className="link">
-            <i class="fas fa-chalkboard-teacher text-4xl"></i>
-            </div>
+<Grid container className="dropdown">
+  <Grid xs={12} item className="closeButton text-right">
+    <IconButton onClick={onClose}>
+      <CloseIcon />
+    </IconButton>
   </Grid>
+  <div className="link">
+    <i class="fas fa-home text-4xl"></i><Link className="icons" to="/">Accueil</Link>
+  </div>
+    <i class="far fa-newspaper text-4xl"></i><Link className="icons" to ="/news">Actualités</Link>
+  <div className="link">
+    <i class="fas fa-clipboard-list ml-2 text-4xl"></i><Link className="icons ml-1" to ="/benefits">Prestations</Link>
+  </div>
+  <div className="link">
+    <i class="fas fa-paint-brush text-4xl"></i><Link className="icons" to ="/beauty-tips">Astuces beauté</Link>
+  </div>
+  <div className="link">
+    <i class="fas fa-image pl-2 text-4xl"></i><Link className="icons" to ="/gallery">Galerie</Link>
+  </div>
+  <div className="link">
+    <i class="fas fa-chalkboard-teacher text-4xl"></i>
+  </div>
+</Grid>
 );
 
 export default DropdownInterfaceView;
