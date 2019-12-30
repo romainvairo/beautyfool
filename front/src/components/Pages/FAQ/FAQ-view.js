6144d9c96edcfc53743e65275ba20019313abb23
romainvairo <romain.vairo@hotmail.com>
2019-12-30 16:50:59 +0100

    FAQ

--- front/src/components/Pages/FAQ/FAQ-view.js
+++ front/src/components/Pages/FAQ/FAQ-view.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
-const FAQView = ({ translations }) => (
-  <div></div>
+import './FAQ.scss';
+
+const FAQView = ({ translations, faq }) => (
+  faq.map((questions) => (
+     <div className="container-fluid flex flex-col pb-6 w-full">
+      <h1 className="text-black text-center text-2xl font-bold">{questions.question}</h1>
+      <p className="answer bg-gray-300">{questions.answer}</p>
+      <hr className="self-center border-black mt-12 w-64"/>
+    </div>
+  ))
+
 );
 
 export default FAQView;
