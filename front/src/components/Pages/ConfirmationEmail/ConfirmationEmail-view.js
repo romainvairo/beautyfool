29920c97176fb80baf7d63ddb5fcebc2f8312c02
FrancoisRocket <darbon.francois@gmail.com>
2019-12-30 19:27:42 +0100

    correction confirmation emauil

--- front/src/components/Pages/ConfirmationEmail/ConfirmationEmail-view.js
+++ front/src/components/Pages/ConfirmationEmail/ConfirmationEmail-view.js
@@ -6,7 +6,7 @@ import './ConfirmationEmail.scss';
 
 const ConfirmationEmailView = ({ messageName, translations }) => (
     <div className="container-fluid flex flex-col pb-6 w-full">
-      <p className="answer bg-gray-300 text-center">Inscription terminée, votre email est validé avec succès</p>
+      <p className="answer bg-gray-300 text-center">{translations.message[messageName]}</p>
       <hr className="self-center border-black mb-6 mt-6 w-64"/>
     <Button component={Link} style={{
         backgroundColor: "black",
