4c85c158fb11f3d60263c1c8661d17d1718e70b5
Denis Guimaraes <guimaraes.dev@gmail.com>
2020-02-27 22:06:06 +0100

    correction pour mise en production sur heroku du back

--- back/config/server.js
+++ back/config/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const http = require('http');
+const cors = require('cors');
 
 const { debug } = require('./debug');
 
@@ -7,10 +8,16 @@ const port = 8000;
 
 // create an app express to handle the http server
 const app = express();
+// cors origin URL - Allow inbound traffic from origin
+const corsOptions = {
+  origin: true,
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(cors());
 const server = new http.Server(app);
 
 // make the server listen on a specific port
-server.listen(port, () => debug.server(`Http server listening on port ${port}`));
+server.listen(process.env.PORT || port);
 
 module.exports = {
   server,
